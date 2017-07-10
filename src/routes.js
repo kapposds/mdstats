f7d76b507d0302e44ddc197249487a8cddf63047
koniaris.giwrgos@gmail.com <george@localhost.localdomain>
2017-07-10 20:59:59 +0300

    send fake cpu numbers

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,17 @@ const router = express.Router()
 
 router.get('/ping',function (req, res) {
     res.json({
-    	status: 'live'
+    	serverStatus: 'green',
+    	mongoDBStatus: 'green',
+
+    })
+})
+
+router.get('/cpu',function (req, res) {
+    res.json({
+    	status: 'green',
+    	value: Math.random() * (100 - 1) + 1
+    	
     })
 })
 
