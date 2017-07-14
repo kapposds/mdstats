f0493444acc4e06adfadd7aa05a926ae97f2fd71
koniaris.giwrgos@gmail.com <george@localhost.localdomain>
2017-07-14 19:30:36 +0300

    rename

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import StatisticsService from './Stats/StatisticsService'
+import StatisticsService from './Services/StatisticsService'
 
 const router = express.Router()
 
